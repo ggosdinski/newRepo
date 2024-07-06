0b7b2d3206179c4e2f30f1a627d5e391a3f5bf9a
ggosdinski <gianfranco.gosdinski@icloud.com>
2024-07-06 09:24:16 -0500

    Clientside-Validation

--- server.js
+++ server.js
@@ -2,42 +2,51 @@
  * This server.js file is the primary file of the 
  * application. It is used to control the project.
  *******************************************/
+
 /* ***********************
  * Require Statements
  *************************/
-const express = require("express")
-const expressLayouts = require("express-ejs-layouts")
-const env = require("dotenv").config()
-const app = express()
-const static = require("./routes/static")
-const baseController = require("./controllers/baseController")
-const inventoryRoute = require("./routes/inventoryRoute")
-const utilities = require('./utilities/')
-const session = require("express-session")
-const pool = require('./database/')
-const accountRoute = require('./routes/accountRoute');
+const express = require("express");
+const expressLayouts = require("express-ejs-layouts");
+const env = require("dotenv").config();
+const session = require("express-session");
+const bodyParser = require("body-parser");
+const flash = require("connect-flash");
+const pgSession = require("connect-pg-simple")(session);
+const pool = require("./database/");
+const utilities = require("./utilities/");
+const staticRoutes = require("./routes/static");
+const baseController = require("./controllers/baseController");
+const inventoryRoute = require("./routes/inventoryRoute");
+const accountRoute = require("./routes/accountRoute");
+/* const flash = require('connect-flash');
+const session = require('express-session'); */
+
+const app = express();
 
 /* ***********************
  * View Engine and Templates
  *************************/
-app.set("view engine", "ejs")
-app.use(expressLayouts)
-app.set("layout", "./layouts/layout") // not at views root
+app.set("view engine", "ejs");
+app.use(expressLayouts);
+app.set("layout", "./layouts/layout"); // not at views root
 
 /* ***********************
  * Middleware
- * ************************/
+ *************************/
 app.use(session({
-  store: new (require('connect-pg-simple')(session))({
-    createTableIfMissing: true,
+  store: new pgSession({
     pool,
+    createTableIfMissing: true,
   }),
   secret: process.env.SESSION_SECRET,
   resave: true,
   saveUninitialized: true,
   name: 'sessionId',
-}))
+}));
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // Express Messages Middleware
 app.use(require('connect-flash')())
@@ -46,25 +55,24 @@ app.use(function(req, res, next){
   next()
 })
 
-
 /* ***********************
  * Routes
  *************************/
-app.use(static)
+app.use(staticRoutes);
 
 // Index route
 app.get("/", utilities.handleErrors(baseController.buildHome));
 
 // Inventory routes
-app.use("/inv", inventoryRoute)
+app.use("/inv", inventoryRoute);
 
 // Account routes
 app.use('/account', accountRoute);
 
 // Middleware for handling 404 errors (Page Not Found)
 app.use(async (req, res, next) => {
-  next({ status: 404, message: 'Sorry, we appear to have lost that page.' })
-})
+  next({ status: 404, message: 'Sorry, we appear to have lost that page.' });
+});
 
 /* ***********************
  * Express Error Handler
@@ -81,18 +89,16 @@ app.use(async (err, req, res, next) => {
   });
 });
 
-
-
 /* ***********************
  * Local Server Information
  * Values from .env (environment) file
  *************************/
-const port = process.env.PORT
-const host = process.env.HOST
+const port = process.env.PORT || 3000;
+const host = process.env.HOST || 'localhost';
 
 /* ***********************
  * Log statement to confirm server operation
  *************************/
 app.listen(port, () => {
-  console.log(`app listening on ${host}:${port}`)
-})
+  console.log(`App listening on ${host}:${port}`);
+});
