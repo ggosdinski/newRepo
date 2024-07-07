3be3eecb8d9e30b1c3c8a2b7fb9fb99b8d30c778
ggosdinski <109007954+ggosdinski@users.noreply.github.com>
2024-07-06 20:28:57 -0500

    Add files via upload

---
+++ routes/accountRoute.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const router = express.Router();
+const utilities = require("../utilities/");
+const accountController = require("../controllers/accountController");
+const regValidate = require('../utilities/account-validation')
+
+// Route to display login view
+router.get("/login", accountController.buildLogin);
+
+// Route to display registration view
+router.get("/register", accountController.buildRegister);
+
+// Route to process registration form submission
+router.post(
+    "/register",
+    regValidate.registationRules(),
+    regValidate.checkRegData,
+    utilities.handleErrors(accountController.registerAccount)
+  )
+
+// Handler for intentional error
+router.get("/intentional-error", (req, res, next) => {
+    next(new Error("This is an intentional 500 error."));
+});
+
+// Process the login attempt
+router.post(
+    "/login",
+    (req, res) => {
+      res.status(200).send('login process')
+    }
+  )
+module.exports = router;
